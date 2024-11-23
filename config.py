58a70203a6d0c67176a89a56fd3b2c5ffcabd5e8
YaexMiko <aloksinghmains756@gmail.com>
2024-11-23 13:46:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "6943832981"))
 #port set to default 8080
 PORT = os.environ.get("PORT", "7993")
 #your database url mongodb only You can use mongo atlas free cloud database
-DB_URL = os.environ.get("DB_URL", "mongodb+srv://Dark:aloksingh@k@dark.sfhmd.mongodb.net/?retryWrites=true&w=majority")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://Dark:aloksingh@cluster0.i9siz.mongodb.net/?retryWrites=true&w=majority")
 #your database name
 DB_NAME = os.environ.get("DB_NAME", "Dark")
 
